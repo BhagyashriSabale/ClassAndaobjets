bffd128e1fe9582c0ecfab4a7012baca8d60ef99
BhagyashriSabale <balisabale16@gmail.com>
2023-06-24 09:20:39 +0530

    [Bhagyashri] Welcome Programs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassAndObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome Class & Objects examples");
+        }
+    }
+}
