90a39a4141d0edb0caa0a29c7624067478a53426
BhagyashriSabale <balisabale16@gmail.com>
2023-06-24 09:38:42 +0530

    [Bhagyashri] find even numbers out of first 100 numbers

---
+++ EvenNumbers2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class EvenNumbers2
+    {
+        public  void EvenNum()
+        {
+            Console.WriteLine("Even numbers between 1 and 100:");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,13 +7,17 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Welcome Class & Objects examples");
             Console.WriteLine("Choose options in below programs:");
-            Console.WriteLine("\n1.AdditionFirst50Num");
+            Console.WriteLine("\n1.AdditionFirst50Num\n2.EvenNumbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     Add50Num1 add50Num1 = new Add50Num1();
                     add50Num1.Addition50Num();
+                    break;
+                    case 2:
+                    EvenNumbers2 evenNumbers2 = new EvenNumbers2();
+                    evenNumbers2.EvenNum();
                     break;
             }
         }
