305a1d87efde73741b9286cd3dd291f112ec89e9
BhagyashriSabale <balisabale16@gmail.com>
2023-06-24 09:32:35 +0530

    [Bhagyashri] Add first 50 numbers

---
+++ Add50Num1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class Add50Num1
+    {
+        public void Addition50Num()
+        {
+            int sum = 0;
+            for (int i = 0; i <=50; i++) 
+            {
+                sum += i;
+            }
+            Console.WriteLine("The sum of the first 50 numbers is: " + sum);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Welcome Class & Objects examples");
+            Console.WriteLine("Choose options in below programs:");
+            Console.WriteLine("\n1.AdditionFirst50Num");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Add50Num1 add50Num1 = new Add50Num1();
+                    add50Num1.Addition50Num();
+                    break;
+            }
         }
     }
 }
